3e27a2ce5ecda43fe991ba0b6cb4961adebccb1a
rifat-54 <rifat999por@gmail.com>
2026-05-25 21:27:21 +0600

    impliment better auth

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import express from "express"
 import cors from "cors"
 import routes from "./routes"
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
 const app=express()
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin:process.env.FRONTEND_URL,
+    credentials:true
+}))
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
 
 app.use("/api/v1",routes)
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,10 @@ import userRouter from "../modules/user/user.route";
 import equipmentRouter from "../modules/equipment/equipment.route";
 import logRouter from "../modules/usage-log/log.route";
 
+
 const routes=Router()
+
+
 
 routes.use("/user",userRouter)
 routes.use("/equipment",equipmentRouter)
